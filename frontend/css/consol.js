5c79dc76215977faabfe17dbbe2c8831c61b4096
KEER212 <114868403+KEER212@users.noreply.github.com>
2022-10-04 00:54:52 +0300

    Add files via upload

---
+++ frontend/css/consol.js
@@ -0,0 +1,69 @@
+
+//let &é = 44 ;
+//let éé = 11;
+//let a = 100;
+//let b = 150
+
+//a = a + b // 250
+//b = a - b // 100
+//a = a - b // 150
+
+//console.log(a,b)
+
+
+
+//    ДЗ
+//1 a = ""
+//2 pi = 3.14
+//let a = 20
+//let b = 70
+//let c = 60
+// console.log(c + (a - b ));
+
+
+//let neme = prompt('Имя');
+//let address = prompt('Адрес');
+//let phone = prompt('Номер телефона');
+
+
+
+//switch case break
+
+//console.log('Мое имя -'  + neme + '.Где я живу -' + address + 'Мой номер  телефона - '+ phone )
+
+//let kurs = 58 ;
+//let rub = 580;
+
+//console.log( rub/kurs );
+
+//let a = 5;
+//let b = 10;
+//let c = +a + b--;
+////console.log(c);
+let code = '10'
+let b = '9'
+if(b < code){
+    console.log('Констата больше 10')
+}else{
+    console.log('Констата менше 10');
+}
+
+teg = 'p'
+
+switch(teg){
+    case('div'):
+    console.log('div');
+    break;
+
+    case('p'):
+    console.log('p');
+    break;
+
+    case('h1'):
+    console.log('h1');
+    break;
+
+    case('span'):
+    console.log('span');
+    break;
+}
---
+++ frontend/js/revival.js
@@ -0,0 +1,28 @@
+function kod(a, b){
+b = 10
+    if(a > b){
+        console.log('Констата больше 10')
+    }else{
+        console.log('Констата менше 10');
+    }
+
+}
+kod(9)
+function alert(){
+    console.log('Привет,мир');
+}
+alert()
+function js(){
+    console.log('JavaSkript');
+}
+js()
+
+function text(){
+    let text = 'lorem'
+    if(text =='lorem'){
+console.log('увас ощибка в тексте');
+    }else{
+        console.log('содержимое переменной:'+ text);
+    }
+}
+text()
