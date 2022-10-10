ed02aa23c06effd19efe02c9647d184903799db5
KEER212 <114868403+KEER212@users.noreply.github.com>
2022-10-10 21:53:07 +0300

    Add files via upload

--- frontend/js/revival.js
+++ frontend/js/revival.js
@@ -26,76 +26,102 @@
 //}
 //}
 //text()
-let neme ='Kerim'  //string
-const age = 17 //number
-let bj = 15
-if(bj > age){
-console.log('Доступ запрещен');
-}else{
-    console.log(`Привет ${neme}`);
-}
-function viewMoney() {
-    console.log('вот вам 10$');
-}
-viewMoney()
-function getMoney(a){
-    console.log(`вот вам ${a}$`);
-    return(`вот вам ${a} $`)
-}getMoney(212)
-function kub(a){
-    console.log(a*a);
-}kub(121)
-function numer(a, b, c,){
-    if(a > b && a > c){
-        console.log(a);
-    }else if(b > a && b > c){
-        console.log(b);
-    }else if(c > a && c > b){
-        console.log(c);
-    }
-}numer(11, 454, 999,)
-function form(a, b, c){
-    console.log(b ** 2) - 4 * (a * c);
-}form(4, 2, 2)
-  let sum = (num, num2) =>{
-    if(num % 2 == 0 && num2 % 2 == 0 ){
-        return num2 + num
-    } else if(num % 2 !== 0 && num2 % 2 !== 0 ){
-        return num2 + num
-    }else{ 
-        return num % 2 !== 0 ? num : num2
-    }
-  }
+//const age = 17 //number
+//let bj = 15
+//if(bj > age){
+//    //let neme ='Kerim'  //string
+//console.log('Доступ запрещен');
+//}else{
+//    console.log(`Привет ${neme}`);
+//}
+//function viewMoney() {
+//    console.log('вот вам 10$');
+//}
+//viewMoney()
+//function getMoney(a){
+//    console.log(`вот вам ${a}$`);
+//    return(`вот вам ${a} $`)
+//}getMoney(212)
+//function kub(a){
+//    console.log(a*a);
+//}kub(121)
+//function numer(a, b, c,){
+//    if(a > b && a > c){
+//        console.log(a);
+//    }else if(b > a && b > c){
+//        console.log(b);
+//    }else if(c > a && c > b){
+//        console.log(c);
+//    }
+//}numer(11, 454, 999,)
+//function form(a, b, c){
+//    console.log(b ** 2) - 4 * (a * c);
+//}form(4, 2, 2)
+//  let sum = (num, num2) =>{
+//    if(num % 2 == 0 && num2 % 2 == 0 ){
+//        return num2 + num
+//    } else if(num % 2 !== 0 && num2 % 2 !== 0 ){
+//        return num2 + num
+//    }else{ 
+//        return num % 2 !== 0 ? num : num2
+//    }
+//  }
+//
+// // console.log(sum(3, 3));
+////let man = {
+// //   neme : 'kerim Ars',
+// //   boz : '17' ,
+ //   rost : '175cm',
+ //   emil : 'rerim095@gmail.com' ,
+ //   pasword : '313233',
+ //   money : '100000RUB' ,
+ //   ed : function () {
+ //       if(man.pasword == ker ){
+   //         console.log(`Добро пожаловать ${man.neme}`);
+ //       }else{
+ //           console.log(`Указанный пороль ${ker} неверен.Попробуйте ещё раз .`);
+ //       }
+  //  },
+  //  /////////////////////
+ //   address: {
+  //  republik : 'Чеченская республика',
+  //  city : 'Город Грозный',
+  //  street:'Горная ул.33',
+  //  },
+//}
+//let ker = '313233';
+// function ed () {
+ //   if(man.pasword == ker ){
+ //       console.log(`Добро пожаловать ${man.neme}`);
+ //   }else{
+ //       console.log(`Указанный пороль ${ker} неверен.Попробуйте ещё раз .`);
+  //  }
+//}
+//ed()
+//console.log(man);
 
- // console.log(sum(3, 3));
-let man = {
-    neme : 'kerim Ars',
-    boz : '17' ,
-    rost : '175cm',
-    emil : 'rerim095@gmail.com' ,
-    pasword : '313233',
-    money : '100000RUB' ,
-    ed : function () {
-        if(man.pasword == ker ){
-            console.log(`Добро пожаловать ${man.neme}`);
-        }else{
-            console.log(`Указанный пороль ${ker} неверен.Попробуйте ещё раз .`);
-        }
-    },
-    /////////////////////
-    address: {
-    republik : 'Чеченская республика',
-    city : 'Город Грозный',
-    street:'Горная ул.33',
-    },
-}
-let ker = '313233';
- function ed () {
-    if(man.pasword == ker ){
-        console.log(`Добро пожаловать ${man.neme}`);
-    }else{
-        console.log(`Указанный пороль ${ker} неверен.Попробуйте ещё раз .`);
-    }
-}
-ed()
-console.log(man);
+let a = ['lorem', 123, true, 'ipsum'];
+console.log(a.length);
+console.log(a[a.length - 1]);
+console.log(a[0]);
+a.unshift('212121')
+a.unshift(122)
+a.unshift(4)
+a.shift()
+a.pop()
+a.pop()
+console.log(a);
+///////////////////////////////////////////::::
+let em = [];
+em.unshift('kerim')
+em.unshift(212)
+em.unshift(true)
+em.unshift('lorem')
+em.unshift(2005)
+console.log(em);
+/////////////////////////////////////::::::::::
+let number =[1, 3, 5, 7, 9];
+em.splice(0, 5, '2','6','10','14','18')
+em.splice(1, 3, '4','4','4')
+console.log(em);
+
